38044db29ca4fa3591e678b6e5d05fdb073a9caf
Lucas <lpmendonca@minha.fag.edu.br>
2025-03-07 20:51:49 -0300

    Aula 1

---
+++ Alunos/Lucas/Aula 1/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        printHola();
+    }
+
+    public static void printHola() {
+        System.out.println("Hola que Tal");
+    }
+}
