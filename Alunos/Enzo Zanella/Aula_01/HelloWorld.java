cce19875d67fee130bb8647a0524bc258e812963
Enzo <edzfranceschini@minha.fag.edu.br>
2025-03-09 17:42:01 -0300

    Aula 1

--- Alunos/Enzo Zanella/Aula_01/HelloWorld.java
+++ Alunos/Enzo Zanella/Aula_01/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        olaMundo();
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá, Mundo!");
     }
 }
